fe9fd6d9d5f468c2f4f5bc283f43f81db7ef91be
Archimedes Firmino <archimedessfirmino@gmail.com>
2023-04-11 21:46:22 -0300

    Ajuste na página de contato.

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,11 +1,33 @@
 $(document).ready(myContacts)
 
 /**
- * Função principal da página "home".
+ * Função principal da página "contacts".
  **/
 function myContacts() {
     /**
-     * Altera o título da página quando 'home' for acessada.
+     * Altera o título da página quando 'contacts' for acessada.
      **/
-    changeTitle('Contate-nos')
+    changeTitle('Faça contato')
+
+    $(document).on('submit', '#cForm', sendContact)
+
+}
+
+function sendContact(ev) {
+
+    var formJSON = {}
+    console.log('++++', ev.target)
+    const formData = new FormData(ev.target);
+    console.log('>>>', formData)
+    for (const [key, value] of formData) {
+        formJSON[key] = value
+    }
+
+    formJSON = JSON.stringify(formJSON)
+
+    console.log(formJSON)
+
+    // console.log(JSON.parse(formJSON))
+
+    return false
 }
