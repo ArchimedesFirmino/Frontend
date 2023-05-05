d0762605359ff941ac5b90894f7d633e91dbed9f
Archimedes <archimedessfirmino@gmail.com>
2023-05-04 21:46:40 -0300

    Mudanças no View dos artigos.

--- pages/home/index.js
+++ pages/home/index.js
@@ -15,11 +15,7 @@ function myHome() {
      **/
     changeTitle()
 
-    /**
-     * Quando clicar em um artigo.
-     **/
-    $(document).on('click', '.art-item', loadArticle)
-
+ 
     var articleList = '';
 
     /**
@@ -54,14 +50,3 @@ function myHome() {
 
 }
 
-/**
- * Carrega o artigo completo.
- */
-function loadArticle() {
-
-    // Obtém o id do artigo e armazena na sessão.
-    sessionStorage.article = $(this).attr('data-id')
-
-    // Carrega a página que exibe artigos → view.
-    loadpage('view')
-}
